cc2935f8f66336c294a09963444791c7d5e13ed9
diego.leguizamon <diegog_legui@hotmail.com>
2023-01-25 12:04:43 -0300

    agregados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,18 @@ export class AppComponent {
   operA: number = 0;
   operB: number = 0;
   resultado: number = 0;
+  negativo: string =''
 
   sumar():void{
     this.resultado = this.operA + this.operB;
+    this.negativo ='';
   }
   restar():void{
     this.resultado = this.operA - this.operB;
+    if(this.resultado < 0){
+      this.negativo = 'El resultado es negativo';
+    }else{
+      this.negativo = 'El resultado es positivo';
+    }
   }
 }
